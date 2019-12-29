bd903075e8b50519f93970939b339ea6e7163ee6
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-12-29 18:27:00 +0000

    Added the message Form

--- client/src/Component/Messages.jsx
+++ client/src/Component/Messages.jsx
@@ -1,42 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Message extends Component {
-    state = { 
-        data: [{ id: 1, value: 0, Message: "Hi" }],
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: ""
+    };
+  }
 
-     }
-    render() { 
-        return ( 
+  handleSubmit = event => {
+    event.preventDefault();
+    const data = this.state;
+    console.log("Final Data is",data);
+  };
 
-            <div>
-                <h1>Sneaky Chat</h1>
+  handleInputChange = event => {
+    event.preventDefault();
+    this.setState({
+      message: event.target.value
+      // [event.target.message]: event.target.value
+    });
+  };
 
-                <button
-                    className="btn btn-primary btn-sm m-2">
-                </button>
+  render() {
+    const { message } = this.state;
+    return (
+      <div>
+        <h1>Sneaky Chat</h1>
+        <label>Enter Message</label>
+        <p>Your Message is: {message}</p>
+        <form onSubmit={this.handleSubmit}>
+          <p>
+            <input
+              type="text"
+              placeholder="Your Message"
+              message="Your Message"
+              onChange={this.handleInputChange}
+              value={message}
+            />
+          </p>
+          <p>
+            <button>Send Message</button>
+          </p>
 
-                <form id="userForm">
-                    <label>Enter Message</label>
-                    <textarea class="form-control" id="message"></textarea>
-                    <br />
-                    <input
-                    type="submit"
-                    class="btn btn-primary"
-                    value="Send Message"
-                    />
-                </form>
-
-
-
-            </div>
-         );
-    }
+        </form>
+      </div>
+    );
+  }
 }
 
-
-// addInfo(){
-    
-    
-// } 
- 
 export default Message;
