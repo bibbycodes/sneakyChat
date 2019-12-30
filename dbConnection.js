9867ec689fb24c2f7deb306494760edfd1acea28
Robert <bibbycodes@gmail.com>
2019-12-30 18:52:16 +0000

    drops table after each test

--- dbConnection.js
+++ dbConnection.js
@@ -8,40 +8,46 @@ class dbConnection {
     this.port = process.env.DB_PORT;
 
     if (process.env.NODE_ENV == "TEST"){
-      console.log("testing")
       this.user = process.env.DB_USER_LOCAL;
       this.db_name = process.env.TEST_DB_NAME;
       this.db_ip = "localhost"
       this.uri = `postgres://${this.user}@${this.db_ip}:${this.port}/${this.db_name}`;
     } else if (process.env.NODE_ENV == "DEVELOPMENT") {
-      console.log("development")
       this.user = process.env.DB_USER_LOCAL;
       this.db_name = process.env.DB_NAME;
       this.db_ip = "localhost"
       this.uri = `postgres://${this.user}@${this.db_ip}:${this.port}/${this.db_name}`;
     } else if (process.env.NODE_ENV == "PRODUCTION") {
-      console.log("production")
       this.user = process.env.DB_USER;
       this.db_name = process.env.DB_NAME;
       this.db_ip = process.env.DB_IP;
       this.uri = `postgres://${this.user}:${this.password}@${this.db_ip}:${this.port}/${this.db_name}`;
     }
-    console.log(this.uri)
     this.client = new Client(this.uri)
   }
 
-  start() {
-    this.client.connect()
-    console.log("starting db")
+  async start() {
+    try {
+      await this.client.connect()
+    } catch (err) {
+      console.error(err)
+    }
   }
 
-  close() {
-    this.client.end()
-    console.log("closing db")
+  async close() {
+    try {
+      await this.client.end()
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   async query(query) {
-    return await this.client.query(query)
+    try {
+      return await this.client.query(query)
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
--- models/message.js
+++ models/message.js
@@ -1,32 +1,45 @@
 const dbConnection = require('../dbConnection')
 
 class Message {
-  constructor() {
+  constructor(sender, body, conversationId) {
     this.db = new dbConnection()
+    this.senderId = sender.id
+    this.body = body
+    this.conversation_id = conversationId
   }
 
-  async findMessageById(id) {
+  async find(id) {
     let result = await this.db.query(
       `SELECT * FROM messages WHERE id=${id}`
     )
-    
-    return result.rows[0].body
+    return result
+  }
+
+  async create() {
+    return await this.db.query(
+      `INSERT INTO messages (body, sender_id, created_at, conversation_id) 
+      VALUES (${this.body}, ${this.senderId}, ${Date.now()}, ${this.conversationId})`
+    )
   }
 
   async getConvo(user1, user2) {
     let sent = await this.db.query(
-      `SELECT * FROM messages WHERE sender_id=${user1} AND recipient_id=${user2}`
+      `SELECT * FROM messages 
+      WHERE sender_id=${user1} 
+      AND recipient_id=${user2}
+      ORDER BY created_at ASC`
     )
     let received = await this.db.query(
-      `SELECT * FROM messages WHERE sender_id=${user2} AND recipient_id=${user1}`
+      `SELECT * FROM messages 
+      WHERE sender_id=${user2} 
+      AND recipient_id=${user1}
+      ORDER BY created_at ASC`
     )
 
     sent = sent.rows
     received = received.rows
-
     let messages = (sent.concat(received))
     messages.sort((a, b) => (a.created_at > b.created_at) ? 1 : -1)
-
     return messages
   }
 }
