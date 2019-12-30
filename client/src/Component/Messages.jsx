e3ade74ae34df88e77554cbbc50f1d7bdf3351d9
your Shubinder Virk <shubinder92@gmail.co.uk>
2019-12-30 20:55:36 +0000

    Rendering a Collection of Messages

--- client/src/Component/Messages.jsx
+++ client/src/Component/Messages.jsx
@@ -1,32 +1,51 @@
 import React, { Component } from "react";
+// import Navbar from "./Component/navbar";
+
 
 class Message extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: ""
+      message: " ",
+      messages:[]
+
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
-    const data = this.state;
-    console.log("Final Data is",data);
+    const data = this.state.message;
+
+    let msg = this.state.messages
+    
+    msg.push(data)
+
+    this.setState({
+        messages: msg
+    });
+    
+    console.log("Final Data is", msg );
+
   };
 
   handleInputChange = event => {
-    event.preventDefault();
+    // msg = this.state.messages
+    // msg.push(event.target.
+     event.preventDefault();
     this.setState({
       message: event.target.value
-      // [event.target.message]: event.target.value
     });
   };
 
   render() {
     const { message } = this.state;
+    
     return (
       <div>
-        <h1>Sneaky Chat</h1>
+    <h1>Sneaky Chat</h1>
+
+        <h1>{this.state.messages.map((item,i) => <li key={i}>{item}</li>)}</h1>
+
         <label>Enter Message</label>
         <p>Your Message is: {message}</p>
         <form onSubmit={this.handleSubmit}>
@@ -39,14 +58,17 @@ class Message extends Component {
               value={message}
             />
           </p>
+
           <p>
             <button>Send Message</button>
           </p>
-
         </form>
       </div>
     );
   }
 }
+
+
+
 
 export default Message;
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Message from './Component/Messages';
+// import Navbar from './Component/navbar';
 
 import * as serviceWorker from './serviceWorker';
 
