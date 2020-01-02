65b7432ad4be6655826ddd7cf24ade4e85385241
Robert <bibbycodes@gmail.com>
2020-01-02 18:16:02 +0000

    can authenticate a user

--- __tests__/User.test.js
+++ __tests__/User.test.js
@@ -1,6 +1,6 @@
 const User = require('../models/User')
 const Helper = require('../test_helpers/test_helpers')
-process.env.NODE_ENV = "TEST"
+process.env.NODE_ENV = 'TEST'
 
 describe('User', () => {
   let helper = new Helper()
@@ -18,23 +18,39 @@ describe('User', () => {
   describe('.find', () => {
     it('retrieves a user from the database', async () => {
       let result = await User.find(1)
-      expect(result[0].first).toEqual("Robert")
+      expect(result[0].first).toEqual('Robert')
+    })
+
+    it('returns [] if a user doesnt exist', async () => {
+      expect(await User.find(0)).toEqual([])
     })
   })
 
   describe('.create', () => {
     it('creates a new user', async () => {
-      let user = await User.create("Thomas", "Griffith", "tom@gmail.com", 'secret')
+      let user = await User.create('Thomas', 'Griffith', 'tom@gmail.com', 'secret')
       expect(user instanceof User).toBe(true)
     })
 
     it('adds a user to the database', async () => {
       await User.create('Robert', 'Rosiji', 'rob@gmail.com', 'secret')
       let user = await User.find(5)
-      expect(user[0].first).toEqual("Robert")
-      expect(user[0].last).toEqual("Rosiji")
-      expect(user[0].email).toEqual("rob@gmail.com")
-      expect(user[0].password).toEqual("secret")
+      expect(user[0].first).toEqual('Robert')
+      expect(user[0].last).toEqual('Rosiji')
+      expect(user[0].email).toEqual('rob@gmail.com')
+      expect(user[0].password).toEqual('secret')
+    })
+  })
+
+  describe('.authenticate', () => {
+    it('returns the user', async () => {
+      let user = await User.create('Joe', 'Griffith', 'joe@gmail.com', 'testpass')
+      expect(await User.authenticate('joe@gmail.com', 'testpass')).toEqual(user)
+    })
+
+    it('returns undefined if the password is incorrect', async () => {
+      let user = await User.create('Joe', 'Griffith', 'joe@gmail.com', 'testpass')
+      expect(await User.authenticate('joe@gmail.com', 'wrongpass')).toEqual(undefined)
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 const dbConnection = require('../dbConnection')
 
 class User {
-  constructor() {
-
+  constructor(id, first, last) {
+    this.id = id
+    this.first = first
+    this.last = last
   }
 
   static async find(id) {
@@ -24,7 +26,23 @@ class User {
     `)
     let rows = result.rows
     await db.close()
-    return new User(rows[0].first, rows[0].last, rows[0].email, rows[0].password)
+    let user = new User(rows[0].id, rows[0].first, rows[0].last, rows[0].email)
+    // console.log("b", user)
+    return user
+  }
+
+  static async authenticate(email, password) {
+    let db = new dbConnection()
+    await db.start()
+    let result = await db.query(`
+      SELECT * FROM users
+      WHERE email='${email}' AND password='${password}';
+    `)
+    let user = result.rows[0]
+    await db.close()
+    if (user) {
+      return new User(user.id, user.first, user.last)
+    }
   }
 }
 
