0a6e47cfb37b6f0f8843e5909f30c47b8cd3ed47
your Shubinder Virk <shubinder92@gmail.co.uk>
2020-01-28 23:25:46 +0000

    Deleted cypress tests

--- cypress/integration/chatApp.spec.js
+++ cypress/integration/chatApp.spec.js
@@ -1,25 +1,25 @@
-/// <reference types="Cypress" />
+// /// <reference types="Cypress" />
 
-context('Conversation', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000/')
-    });
+// context('Conversation', () => {
+//     beforeEach(() => {
+//       cy.visit('http://localhost:3000/')
+//     });
   
 
-    .click()
-    .type('H').should('have.value', 'H')
+//     .click()
+//     .type('H').should('have.value', 'H')
 
-    .should('have.value', 'slow.typing@email.com')
+//     .should('have.value', 'slow.typing@email.com')
 
 
 
-    it('.submit() - submit a form', () => {
-      // https://on.cypress.io/submit
-      cy.get('.action-form')
-        .find('[type="text"]').type('HALFOFF')
-      cy.get('.action-form').submit()
-        .next().should('contain', 'Your form has been submitted!')
-    })
+//     it('.submit() - submit a form', () => {
+//       // https://on.cypress.io/submit
+//       cy.get('.action-form')
+//         .find('[type="text"]').type('HALFOFF')
+//       cy.get('.action-form').submit()
+//         .next().should('contain', 'Your form has been submitted!')
+//     })
 
-  });
+//   });
 
